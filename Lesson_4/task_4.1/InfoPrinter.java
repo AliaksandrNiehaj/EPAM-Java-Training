f2f75efa0f2a8ecf09484ea9f4bfe79fbf30bfb9
AliaksandrNiehaj <34266753+AliaksandrNiehaj@users.noreply.github.com>
2019-05-26 23:15:28 +0300

    Add files via upload

---
+++ Lesson_4/task_4.1/InfoPrinter.java
@@ -0,0 +1,14 @@
+package by.niakhai.task4_1;
+
+public class InfoPrinter {
+
+	public void getInfo(boolean sign) {
+		
+		if (sign) {
+			System.out.println("All numbers are the same.");
+		} else {
+			System.out.println("All numbers are different.");
+		}
+		
+	}
+}
