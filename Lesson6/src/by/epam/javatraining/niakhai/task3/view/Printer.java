061c16d8aec71b8aa69c93ab2bbc189468aa9959
AliaksandrNiehaj <34266753+AliaksandrNiehaj@users.noreply.github.com>
2019-06-17 02:08:05 +0300

    Delete Printer.java

--- Lesson6/src/by/epam/javatraining/niakhai/task3/view/Printer.java
+++
@@ -1,8 +0,0 @@
-package by.epam.javatraining.niakhai.task3.view;
-
-public class Printer {
-	
-	public static void println(String mes, int i, long factorial) {
-		System.out.printf(mes + "\n", i, factorial);
-	}
-}
